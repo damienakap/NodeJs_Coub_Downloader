7343cb66cc4c1242b8fd86c8baefd7c8b9998411
damienakap <32419883+damienakap@users.noreply.github.com>
2020-05-29 19:29:39 -1000

    Create Shortcut on setup

--- setup.bat
+++ setup.bat
@@ -1,4 +1,19 @@
 @echo off
-cd %cd%
+cd %CD%
+
+set SCRIPT="%CD%\tmp\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%CD%\NodeJs Coub Downloader.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "cmd" >> %SCRIPT%
+echo oLink.Arguments = "/C %CD%\run.bat" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
+
 redist\node-v12.17.0-x64.msi
 npm i
